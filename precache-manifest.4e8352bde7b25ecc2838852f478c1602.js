0b39d98c1cf9243facd10d136e872ece0ab17dcd
chakriyaP <65884346+chakriyaP@users.noreply.github.com>
2020-10-21 15:01:39 +0700

    Updates

---
+++ precache-manifest.4e8352bde7b25ecc2838852f478c1602.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f6eee5c5baf84baec5551d64704a545",
+    "url": "/chakriya-natthanicha-webapp/index.html"
+  },
+  {
+    "revision": "faa857156b768a08f745",
+    "url": "/chakriya-natthanicha-webapp/static/css/main.f0a818d9.chunk.css"
+  },
+  {
+    "revision": "e47b40a5d58fe599cdcc",
+    "url": "/chakriya-natthanicha-webapp/static/js/2.d18f4f1e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chakriya-natthanicha-webapp/static/js/2.d18f4f1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa857156b768a08f745",
+    "url": "/chakriya-natthanicha-webapp/static/js/main.2a547d34.chunk.js"
+  },
+  {
+    "revision": "46d13a14de3ecec7015f",
+    "url": "/chakriya-natthanicha-webapp/static/js/runtime-main.23789834.js"
+  }
+]);
